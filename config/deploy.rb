736791f078efb2c7de69ee2f9926dfd563cccb97
wangyu61 <wangyu61@baidu.com>
2017-05-19 17:36:54 +0800

    bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ task :deploy do
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
+    invoke :'bundle:install --system'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
